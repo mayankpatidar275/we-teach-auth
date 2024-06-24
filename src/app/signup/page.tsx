48d77d92dc912b846487538262c53fe6a9346c69
Mayank Patidar <154582476+thinkmetal-mayank@users.noreply.github.com>
2024-06-24 10:31:40 +0530

    feat: signup login page, db connection

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import { User } from "@/models/userModel";
+import { hash } from "bcrypt";
+import { redirect } from "next/navigation";
+import { connectToDatabase } from "@/lib/utils";
+
+const SignupPage = () => {
+  const signUp = async (formData: FormData) => {
+    "use server";
+    const name = formData.get("name") as string | undefined;
+    const email = formData.get("email") as string | undefined;
+    const password = formData.get("password") as string | undefined;
+
+    if (!name || !email || !password) {
+      throw new Error("Please provide all fields");
+    }
+
+    await connectToDatabase();
+
+    const user = await User.findOne({ email });
+    if (user) throw Error("User already exists");
+
+    const hashedPassword = await hash(password, 10);
+
+    await User.create({ name, email, hashedPassword });
+
+    redirect("/login");
+  };
+  return (
+    <div className="flex justify-center items-center h-dvh">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Welcome,</CardTitle>
+          <CardDescription>Signup to the world of learnings...</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form action={signUp}>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Name</Label>
+                <Input name="name" id="name" placeholder="Name" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  name="email"
+                  type="email"
+                  id="email"
+                  placeholder="demo@gmail.com"
+                />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="password">Password</Label>
+                <Input name="password" id="password" placeholder="demo" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Button type="submit">Sign Up</Button>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between flex-col gap-4">
+          <span>or</span>
+
+          <form action="">
+            <Button type="submit" variant="outline">
+              Login with google
+            </Button>
+          </form>
+
+          <Link href="/signup">Already have an account? Login</Link>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default SignupPage;
--- src/auth.ts
+++ src/auth.ts
@@ -1,40 +1,50 @@
 import NextAuth, { CredentialsSignin } from "next-auth";
-import GoogleProvider from "next-auth/providers/google" 
-import CredentialProvider from "next-auth/providers/credentials"
+import GoogleProvider from "next-auth/providers/google";
+import CredentialProvider from "next-auth/providers/credentials";
 import { User } from "./models/userModel";
 import bcrypt from "bcrypt";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
-    GoogleProvider({ clientId: process.env.GOOGLE_CLIENT_ID, clientSecret: process.env.GOOGLE_CLIENT_SECRET }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     CredentialProvider({
-        name: "Credentials",
-        credentials: {
-          email: { label: "Email", type: "email" },
-          password: { label: "Password", type: "password" },
-        },
-        async authorize(credentials ) {
-          const email = credentials.email as string | undefined;
-          const password = credentials.password as string | undefined;
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const email = credentials.email as string | undefined;
+        const password = credentials.password as string | undefined;
 
-          if(!email || !password) throw new CredentialsSignin("Please provide both email and password");
+        if (!email || !password)
+          throw new CredentialsSignin("Please provide both email and password");
 
-          //connection to db
+        //connection to db
 
-          const user = await User.findOne({email}).select("+password");
+        const user = await User.findOne({ email }).select("+password");
 
-          if(!user) { throw new CredentialsSignin("Invailid email or password");}
- 
-          if(!user.password) { throw new CredentialsSignin("Invailid email or password");}
-          
-          const isPasswordCorrect = await  bcrypt.compare(password, user.password);
+        if (!user) {
+          throw new CredentialsSignin("Invailid email or password");
+        }
 
-          if(!isPasswordCorrect)  { throw new CredentialsSignin("Invailid email or password");}
+        if (!user.password) {
+          throw new CredentialsSignin("Invailid email or password");
+        }
 
-          // Todo: add more complexity like verified (send email to verify)
+        // const isPasswordCorrect = await  bcrypt.compare(password, user.password);
 
-          return {name:user.name, email: user.email, id: user._id} // better not to send user directly because it has password
-        },
-      }),
+        // if (!isPasswordCorrect) {
+        //   throw new CredentialsSignin("Invailid email or password");
+        // }
+
+        // Todo: add more complexity like verified (send email to verify)
+
+        return { name: user.name, email: user.email, id: user._id }; // better not to send user directly because it has password
+      },
+    }),
   ],
-})
+});
